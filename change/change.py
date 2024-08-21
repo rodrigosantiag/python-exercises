2e3db4010a8baaa3999d7c855ce1968a442515c3
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-08-21 05:25:34 -0300

    Add Change exercise

---
+++ change/change.py
@@ -0,0 +1,45 @@
+from typing import List, Optional
+
+
+def find_fewest_coins(coins: List[int], target: int) -> list[int]:
+    if target < 0:
+        raise ValueError("target can't be negative")
+    if target == 0:
+        return []
+
+    target_coin_list = find_fewest_coins_recursive(sorted(coins, reverse=True), target)
+
+    if not target_coin_list:
+        raise ValueError("can't make target with given coins")
+
+    return sorted(target_coin_list)
+
+
+def find_fewest_coins_recursive(
+        coins: List[int], target: int, coin_list: Optional[List[int]] = None
+) -> list[int]:
+    coins = [coin for coin in coins if coin <= target]
+
+    if not coins:
+        return []
+
+    if coin_list is None:
+        coin_list = []
+
+    if target % coins[0] == 0:
+        return coin_list + [coins[0]] * (target // coins[0])
+
+    best_coin_list = []
+    min_length = target // min(coins) + len(coin_list) + 1
+
+    for coin in coins:
+        if len(coin_list) + target // coin < min_length:
+            target_coin_list = find_fewest_coins_recursive(
+                coins, target - coin, coin_list + [coin]
+            )
+
+            if target_coin_list and len(target_coin_list) < min_length:
+                min_length = len(target_coin_list)
+                best_coin_list = target_coin_list
+
+    return best_coin_list
